ada1ab67011630cb98241822d2390fbd5e985c51
jdzwilewska <jdzwilewska@o2.pl>
2019-07-03 00:14:31 +0200

    Zadanie 7.1

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,18 +1,35 @@
 package com.kodilla.stream;
 
-import com.kodilla.stream.forumuser.Forum;
-import com.kodilla.stream.forumuser.ForumUser;
-import java.util.Map;
-import java.util.stream.Collectors;
+import com.kodilla.stream.com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.lambda.ExpressionExecutor;
+import com.kodilla.stream.lambda.Processor;
 
 public class StreamMain {
-    public static void main(String[] args ) {
-        Forum theForum = new Forum();
-        Map <Integer, ForumUser> map = theForum.getUserList().stream()
-                .filter(forumUser -> forumUser.getSex() == 'M')
-                .filter(forumUser -> (2019 - (forumUser.getBirthDate().getYear()) >= 20))
-                .filter(forumUser -> forumUser.getPostCount() > 1 )
-                .collect(Collectors.toMap(forumUser -> forumUser.getUserID(), forumUser -> forumUser ));
-        System.out.println(map);
+    public static void main(String[] args) {
+        ExpressionExecutor expressionExecutor = new ExpressionExecutor();
+        Processor processor = new Processor();
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a + b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a - b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a * b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a / b);
+
+        PoemBeautifier poemBeautifier1 = new PoemBeautifier();
+        poemBeautifier1.beautify("Pierwszy tekst", text -> "ABC" + text + "ABC");
+        poemBeautifier1.beautify("Pierwszy tekst", text -> text.toUpperCase());
+        poemBeautifier1.beautify("Pierwszy tekst", text -> text.replaceAll("tekst", "text"));
+        poemBeautifier1.beautify("Pierwszy tekst", text -> text.replaceAll("\\s", "*"));
+
+        PoemBeautifier poemBeautifier2 = new PoemBeautifier();
+        poemBeautifier2.beautify("Drugi tekst", text -> "ABC" + text + "ABC");
+        poemBeautifier2.beautify("Drugi tekst", text -> text.toUpperCase());
+        poemBeautifier2.beautify("Drugi tekst", text -> text.replaceAll("tekst", "text"));
+        poemBeautifier2.beautify("Drugi tekst", text -> text.replaceAll("\\s", "*"));
+
+        PoemBeautifier poemBeautifier3 = new PoemBeautifier();
+        poemBeautifier3.beautify("Trzeci tekst", text -> "ABC" + text + "ABC");
+        poemBeautifier3.beautify("Trzeci tekst", text -> text.toUpperCase());
+        poemBeautifier3.beautify("Trzeci tekst", text -> text.replaceAll("tekst", "text"));
+        poemBeautifier3.beautify("Trzeci tekst", text -> text.replaceAll("\\s", "*"));
     }
 }
+
